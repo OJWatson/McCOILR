8f1e2ad0be71de72375451ac5dcb8eef0549177f
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-10-07 12:05:38 -0400

    Update Rcpp dependency

--- R/McCOILR.R
+++ R/McCOILR.R
@@ -1,17 +1,18 @@
 #' Rcpp implementation of THE REAL McCOIL
-#' 
+#'
 #' @name McCOILR
 #' @aliases McCOILR package-McCOILR
 #' @docType package
 #' @title Wrapper for THE REAL McCOIL in Rcpp
-#' 
+#'
 #' @importFrom stats median quantile sd
 #' @importFrom utils read.table write.table head
-#' 
-#' @useDynLib McCOILR
-#' 
-#' @description Wrapper for THE REAL McCOIL in Rcpp, so that package can be more easily run on distributed computing services and cluster infrastructure. 
-#' 
+#'
+#' @useDynLib McCOILR, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#'
+#' @description Wrapper for THE REAL McCOIL in Rcpp, so that package can be more easily run on distributed computing services and cluster infrastructure.
+#'
 #' @references 1 Chang H-H, Worby CJ, Yeka A, Nankabirwa J, Kamya MR, Staedke SG, Dorsey G, Murphy M, Neafsey DE, Jeffreys AE, Hubbart C, Rockett KA, Amato R, Kwiatkowski DP, Buckee C, Greenhouse B. 2017. THE REAL McCOIL: A method for the concurrent estimation of the complexity of infection and SNP allele frequency for malaria parasites. PLOS Comput Biol 13: e1005348. doi:10.1371/journal.pcbi.1005348
 #'
 
